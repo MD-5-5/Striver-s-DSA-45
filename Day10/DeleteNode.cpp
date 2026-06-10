1b1c0a67784f3b18fa1dff184a146851dbe281ae
MD-5-5 <devsrivastav64@gmail.com>
2026-06-10 23:21:44 +0530

    Day 10 Solutions

---
+++ Day10/DeleteNode.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
+
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    Solution obj;
+    obj.deleteNode(head->next);
+
+    printList(head);
+
+    return 0;
+}
