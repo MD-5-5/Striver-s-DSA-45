64f1f1a7db4239aa00c507af608558f507b50770
MD-5-5 <devsrivastav64@gmail.com>
2026-06-15 22:12:41 +0530

    Day 15 Solutions

---
+++ Day15/MeetingInOneRoom.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxMeetings(vector<int>& start, vector<int>& end) {
+        vector<tuple<int, int, int>> meetings;
+        for (int i = 0; i < start.size(); i++) {
+            meetings.push_back({end[i], start[i], i + 1}); 
+           
+        }
+
+        sort(meetings.begin(), meetings.end());
+
+        vector<int> result; // To store meeting indices
+        int lastEnd = -1;
+
+        for (auto& m : meetings) {
+            int e = get<0>(m);
+            int s = get<1>(m);
+            int idx = get<2>(m);
+
+            // If meeting starts after last one ends
+            if (s > lastEnd) {
+                // Store index
+                result.push_back(idx); 
+                // Update last end time
+                lastEnd = e; 
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    vector<int> start = {1, 3, 0, 5, 8, 5};
+    vector<int> end   = {2, 4, 6, 7, 9, 9};
+
+    Solution sol;
+    vector<int> res = sol.maxMeetings(start, end);
+
+    for (int idx : res) cout << idx << " ";
+}
